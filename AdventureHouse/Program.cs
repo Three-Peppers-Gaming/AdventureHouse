d537cec990b63db6bcf5818381d07dc3e6cd838b
Steve Sparks <15949542+StevenSSparks@users.noreply.github.com>
2023-01-10 19:32:46 -0600

    Added Setup Project

--- AdventureHouse/Program.cs
+++ AdventureHouse/Program.cs
@@ -8,19 +8,11 @@ namespace AdventureHouse
     internal class Program
     {
 
-
-
-
-
-
-
         static void Main(string[] args)
         {
             Console.Title = "Adventure House";
             Console.SetWindowSize(80, 30);
            
-
-
             // Create and instance of the Adventure Framework Service
             var _advfw = new AdventureFrameworkService(new MemoryCache(new MemoryCacheOptions()), new GetFortuneService());
             PlayAdventureClient.PlayAdventure(_advfw);
